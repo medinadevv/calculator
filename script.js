6b52dc29e275d42dfcf2aded66711308cd904925
medinadevv <gulzina082@gmai.com>
2023-02-13 14:45:55 +0600

    calculator

---
+++ script.js
@@ -0,0 +1,100 @@
+// function sum (a,b ){
+//     return a + b;
+// }
+// function substract (a,b ){
+//     return a - b;
+// }
+// function multyplay (a,b ){
+//     return a * b;
+// }
+// function divdison (a,b ){
+//     return a / b;
+// }
+// const display = document.querySelector("#display");
+// const buttons = document.querySelector("button");
+//
+// buttons.forEach((item) => {
+//     item.onclick = () => {
+//         if (item.id === "clear") {
+//             display.innerHTML = "";
+//         } else if (item.id === "backspace") {
+//             let string = display.innerText.toString();
+//             display.innerText = string.substr(0, string.length - 1)
+//         } else if (display.innerText !== "" && item.id === "equal") {
+//             display.innerText = eval(display.innerText);
+//         } else if (display.innerText !== "" && item.id === "equal") {
+//             display.innerText = "Empty!";
+//             setTimeout(() => (display.innerText = ""), 2000);
+//             {
+//             }
+//         }
+//             else {
+//                 display.innerText += item.id;
+//             }
+//         }
+//
+// })
+//
+//
+// const themeTogglebtn = document.querySelector(".theme-toggle")
+// const calculator = document.querySelector(".calculator")
+// const toggleIcon = document.querySelector(".toggleIcon")
+// let isDark = true;
+// themeTogglebtn.onclick = () => {
+//     calculator.classList.toggle("dark")
+//     themeTogglebtn.classList.toggle("active")
+//     isDark = !isDark;
+// }
+
+
+
+
+
+
+
+const display = document.querySelector('#display')
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if (item.id === 'clear') {
+            display.innerText = '';
+        }
+        else if (item.id === 'backspace') {
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length - 1);
+        }
+        else if (display.innerText !== '' && item.id ===  'equal') {
+            display.innerText = eval(display.innerText);
+        }
+        else if (display.innerText === '' && item.id === 'equal') {
+            display.innerText = "Empty!";
+            setTimeout(() => (display.innerText = '') , 2000);
+        }
+        else {
+            display.innerText += item.id
+        }
+    }
+})
+
+
+const themeToggleBtn = document.querySelector('.theme-toggle');
+const calculator = document.querySelector('.calculator')
+// const toggleIcon = document.querySelector('.toggle-icon')
+
+let isDark = true;
+themeToggleBtn.onclick = () => {
+    calculator.classList.toggle('dark');
+    themeToggleBtn.classList.toggle('active');
+    isDark = !isDark
+}
+
+
+
+
+
+
+
+
+
+
